11be09a92b13931411b508bcac49e61afdfbbf9c
boroosku <102946358+boroosku@users.noreply.github.com>
2022-07-01 13:07:50 +1000

    Add files via upload

---
+++ listenProject/projectListen/appListen/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from .models import SpotMusic
+
+# Register your models here.
+class SpotMusicAdmin(admin.ModelAdmin):
+    list_display = ('user', 'song_author', 'song_title', 'song_image', 'audio', 'created_on')
+admin.site.register(SpotMusic, SpotMusicAdmin)
